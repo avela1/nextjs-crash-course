2e6c07e4d91701d6562a963a479ca4479154b51b
abel <ave968029@gmail.com>
2023-01-03 15:08:33 +0300

    second commit

--- components/ArticleItem.jsx
+++ components/ArticleItem.jsx
@@ -4,12 +4,15 @@ import articleStyle from "./../styles/Article.module.css";
 
 const ArticleItem = ({ article }) => {
   return (
-    <Link href="/article/[id]" as={`/article/${article.id}`}>
-      <a className={articleStyle.card}>
-        <h3>{article.title} &rarr;</h3>
-        <p>{article.body}</p>
-      </a>
-    </Link>
+    (<Link
+      href="/article/[id]"
+      as={`/article/${article.id}`}
+      className={articleStyle.card}>
+
+      <h3>{article.title} &rarr;</h3>
+      <p>{article.body}</p>
+
+    </Link>)
   );
 };
 
